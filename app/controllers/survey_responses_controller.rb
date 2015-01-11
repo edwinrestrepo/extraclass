0e5dc08ffda9c0bddc1b77461e90e5b059260c38
Mixer Gutierrez <mixergutierrez@gmail.com>
2015-01-11 09:32:32 -0500

    Added show correct answers after a survey is submitted

--- app/controllers/survey_responses_controller.rb
+++ app/controllers/survey_responses_controller.rb
@@ -1,14 +1,6 @@
 class SurveyResponsesController < ApplicationController
   before_action :authenticate_student!
   before_action :set_survey
-  #before_action :check_ownership, only: :index
-  #before_action :protect_from_owner, only: :create
-
-  def index
-    @survey_responses = SurveyResponse.get_questions(@survey)
-    @survey_responders = SurveyResponse.get_responders(@survey)
-    @responses_number = SurveyResponse.responses_number(@survey)
-  end
 
   def create
     answers = params[:survey_responses][:questions_answers]
@@ -16,12 +8,7 @@ class SurveyResponsesController < ApplicationController
       SurveyResponse.create(survey_id: @survey.id, question_id: question, answer_id: answer, student_id: current_student.id, timestamp: Time.now.to_i)
     end
     #NotificationsMailer.new_survey_response_email(@survey).deliver
-
-    #@grade = SurveyResponse.get_grade(@survey.id, current_student.id)
-    #render :thanks
-  end
-
-  def thanks
+    render :survey_results
   end
 
   private
@@ -35,11 +22,4 @@ class SurveyResponsesController < ApplicationController
       params.require(:survey_responses).permit(:questions_answers)
     end
 
-    def protect_from_owner
-      redirect_to surveys_url, notice: "You're not allowed to send responses to this survey" if current_user.author?(@survey)
-    end
-
-    def check_ownership
-      redirect_to surveys_url, notice: "You're not allowed to see the responses of this survey" unless current_user.author?(@survey)
-    end
 end
--- app/models/survey_response.rb
+++ app/models/survey_response.rb
@@ -3,4 +3,12 @@ class SurveyResponse < ActiveRecord::Base
   belongs_to :question
   belongs_to :answer
   belongs_to :student
+
+  scope :get_last_response_from_student, ->(survey_id, student_id) { where("survey_id = ? AND student_id = ?", survey_id, student_id).last }
+  scope :get_questions, ->(survey_id) { where("survey_id = ?", survey_id).select(:question_id).distinct }
+
+  def self.get_correct_answers(survey_id, student_id)
+    last_response = get_last_response_from_student(survey_id, student_id)
+    correct_answers_last_submit = joins(:answer).where("survey_id = ? AND student_id = ? AND answers.correct = ? AND timestamp = ?", survey_id, student_id, true, last_response.timestamp).count
+  end
 end
